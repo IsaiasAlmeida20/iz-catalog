337c573c5b11d7f182b574f12f2fc514b60e109b
Isaias Almeida <isaiasalmeida319@gmail.com>
2022-12-26 07:28:06 -0300

    :recycle: refactor: Implementing DELETE method

--- src/main/java/br/com/izan/services/CategoryService.java
+++ src/main/java/br/com/izan/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.izan.dto.CategoryDTO;
 import br.com.izan.entities.Category;
 import br.com.izan.repositories.CategoryRepository;
+import br.com.izan.services.exceptions.DatabaseException;
 import br.com.izan.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 
@@ -49,7 +52,17 @@ public class CategoryService {
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id no found " + id);
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
 		}
 	}
 	
