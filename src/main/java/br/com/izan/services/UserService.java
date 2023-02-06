08a890b2285ac2cefacecaaf903a0b3c71217c96
Isaias Almieda <isaiasalmeida319@gmail.com>
2023-02-06 16:10:35 -0300

    :sparkles: feat: OAuth2 authorization server

--- src/main/java/br/com/izan/services/UserService.java
+++ src/main/java/br/com/izan/services/UserService.java
@@ -108,9 +108,9 @@ public class UserService implements UserDetailsService {
 		User user = repository.findByEmail(username);
 		if(user == null) {
 			logger.error("User not found" + username);
-			throw new UsernameNotFoundException("Email not found");
+			throw new UsernameNotFoundException("Email not found: ");
 		}
-		logger.info("User found" + username);
+		logger.info("User found: " + username);
 		return user;
 	}
 	
